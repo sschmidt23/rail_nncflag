ef5afef069f95a9f13480738fc7c30f18a7b3f68
sschmidt23 <sschmidt@physics.ucdavis.edu>
2025-09-04 10:54:24 -0700

    add nnc files

--- README.md
+++ README.md
@@ -1,44 +1,8 @@
-# rail_bpz
+# rail_nncflag
 
-[![codecov](https://codecov.io/gh/LSSTDESC/rail_bpz/branch/main/graph/badge.svg)](https://codecov.io/gh/LSSTDESC/rail_bpz)
-[![PyPI](https://img.shields.io/pypi/v/bpz?color=blue&logo=pypi&logoColor=white)](https://pypi.org/project/pz-rail-bpz/)
+Implementation of the neural-net-based photo-z quality flag code originally written by Adam Broussard and updated by Irene Moskowitz, adapted for use with RAIL.  This code implements only the basic version, which takes in a set of photometric bands, a point estimate redshift, and true redshifts for a training file, and uses as features the reference band magnitude and the galaxy colors to train a set of neural nets.  These models are applied to the same features in a test set, and the average of the quality scores is returned as a single number ranging from 0(likely bad) to 1(likely good).
 
+The original code read in "zconf", a measure of how peaked the PDF distribution is, but did not use it.  I added an option `include_odds` that calculates ODDS, the integral of the amount of posterior probability in an interval +/- 0.06*(1+zmode) around the PDF mode.  ODDS measures the "peakiness" of the distribution, narrow unimodal PDFs will have a high ODDS value, while multimodal and broad unimodal PDFs will have lower ODDS values.  Setting `include_odds` to `True` will include ODDS as an additional parameter in the neural net training, and may improve reliability.
 
-RAIL interface to BPZ algorithms via the [DESC_BPZ](https://github.com/LSSTDESC/DESC_BPZ) package implementation (also available via PyPI with `pip install desc-bpz`).  Anyone using BPZ via either rail_bpz or DESC_BPZ should cite both [Benitez (2000)](https://ui.adsabs.harvard.edu/abs/2000ApJ...536..571B/abstract) and [Coe et al. (2006)](https://ui.adsabs.harvard.edu/abs/2006AJ....132..926C/abstract).
+Currently, I have the code set up using the inform/estimate Estimator base classes.  This might change in future versions.
 
-In addition to the "default" BPZ CWWSB SED template set, `rail_base` now also includes an additional set of 31 template SEDs, a set that was used to compute the COSMOS 30-band photo-z's using LePhare.  The template set consists of empirical templates from [Poletta et al 2007](https://arxiv.org/pdf/astro-ph/0703255) supplemented with blue [BC03](https://ui.adsabs.harvard.edu/abs/2003MNRAS.344.1000B/abstract) SEDs, see [Ilbert et al 2009](https://ui.adsabs.harvard.edu/abs/2009ApJ...690.1236I/abstract) and [Dahlen et al 2013](https://ui.adsabs.harvard.edu/abs/2013ApJ...775...93D/abstract) for details.  The demo notebook [BPZ_lite_demo.ipynb](https://github.com/LSSTDESC/rail/blob/main/examples/estimation_examples/BPZ_lite_demo.ipynb) in the rail examples repository shows an example of how to use this alternate template set.
-
-As the "lite" name implies, not all features of BPZ are implemented, the main product is the marginalized redshift PDF, which is output for a sample as a `qp` ensemble.  However, several other quantities are computed and stored as "ancillary" data and stored with the ensemble, these are:
-- zmode (float): the mode of the marginalized posterior redshift PDF distribution.
-- zmean (float): the mean of the marginalized posterior redshift PDF distribution.
-- tb (int): the integer index for the "best-fit" SED template **at the redshift mode, `zmode`**.  Note that the best-fit template can be different at different redshifts as the SED observed colors change with redshift, so you **can not** assume this single SED for the full marginalized PDF, it should only be used for the "point estimate" redshift `zmode`.
-- todds (float): relating to the comment above on tb, `todds` is a new quantity not included with the original BPZ implementation, it is the fraction of marginalized posterior probability assigned to `tb`.  So, high values of `todds` would mean that no other templates fit well, even at other redshifts, while a lower value of `todds` means that there are alternative fits, either at the same redshift or other redshifts.  If you are wanting to compute physical quantities based on `tb`, a lower value of `todds` would mean that such fits would be missing degenerate SED solutions, and should not be trusted. 
-
-
-## Contributing
-
-The greatest strength of RAIL is its extensibility; those interested in contributing to RAIL should start by consulting the [Contributing guidelines](https://rail-hub.readthedocs.io/en/latest/source/contributing.html) on ReadTheDocs.
-
-## Citing RAIL
-
-RAIL is open source and may be used according to the terms of its [LICENSE](https://github.com/LSSTDESC/RAIL/blob/main/LICENSE) [(BSD 3-Clause)](https://opensource.org/licenses/BSD-3-Clause).
-If you used RAIL in your study, please cite this repository <https://github.com/LSSTDESC/RAIL>, and RAIL Team et al. (2025) <https://arxiv.org/abs/2505.02928>
-```
-@ARTICLE{2025arXiv250502928T,
-       author = {{The RAIL Team} and {van den Busch}, Jan Luca and {Charles}, Eric and {Cohen-Tanugi}, Johann and {Crafford}, Alice and {Crenshaw}, John Franklin and {Dagoret}, Sylvie and {De-Santiago}, Josue and {De Vicente}, Juan and {Hang}, Qianjun and {Joachimi}, Benjamin and {Joudaki}, Shahab and {Bryce Kalmbach}, J. and {Kannawadi}, Arun and {Liang}, Shuang and {Lynn}, Olivia and {Malz}, Alex I. and {Mandelbaum}, Rachel and {Merz}, Grant and {Moskowitz}, Irene and {Oldag}, Drew and {Ruiz-Zapatero}, Jaime and {Rahman}, Mubdi and {Rau}, Markus M. and {Schmidt}, Samuel J. and {Scora}, Jennifer and {Shirley}, Raphael and {St{\"o}lzner}, Benjamin and {Toribio San Cipriano}, Laura and {Tortorelli}, Luca and {Yan}, Ziang and {Zhang}, Tianqing and {the Dark Energy Science Collaboration}},
-        title = "{Redshift Assessment Infrastructure Layers (RAIL): Rubin-era photometric redshift stress-testing and at-scale production}",
-      journal = {arXiv e-prints},
-     keywords = {Instrumentation and Methods for Astrophysics, Cosmology and Nongalactic Astrophysics, Astrophysics of Galaxies},
-         year = 2025,
-        month = may,
-          eid = {arXiv:2505.02928},
-        pages = {arXiv:2505.02928},
-          doi = {10.48550/arXiv.2505.02928},
-archivePrefix = {arXiv},
-       eprint = {2505.02928},
- primaryClass = {astro-ph.IM},
-       adsurl = {https://ui.adsabs.harvard.edu/abs/2025arXiv250502928T},
-      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
-}
-```
-Please consider also inviting the developers as co-authors on publications resulting from your use of RAIL by [making an issue](https://github.com/LSSTDESC/rail/issues/new/choose).
---
+++ src/rail/estimation/algos/nnc_flag.py
@@ -0,0 +1,341 @@
+"""
+Implementaiton of neural-network quality flag code
+originally written by Adam Broussard, adapted by
+Irene Moskowitz
+
+The code trains a neural net on photometry, specz,
+a measure of photo-z "width", and a measure of how
+peaked the PDF is (zconf/ODDS-like), and returns a
+flag valued between 0 (likely bad redshift) and 1
+(likely good redshift)
+"""
+
+import os
+import numpy as np
+import glob
+import qp
+import tables_io
+from ceci.config import StageParameter as Param
+from rail.core.data import DataHandle, ModelHandle, QPHandle, TableHandle
+from rail.estimation.estimator import CatEstimator, CatInformer
+from rail.utils.path_utils import RAILDIR
+from rail.core.common_params import SHARED_PARAMS
+from keras.models import Sequential
+from keras.layers import Dense
+#from keras.models import model_from_yaml
+from keras.models import model_from_json
+#from keras.callbacks.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.optimizers import Adam # heh
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.losses import Huber
+
+default_node_counts = [100, 200, 100, 50, 1]
+default_activations = ['selu', 'selu', 'selu', 'selu', 'sigmoid']
+
+class NNFlagInformer(CatInformer):
+    """Train the neural network classifier that creates the flags    
+    """
+    name = "NNFlagInformer"
+    config_options = CatInformer.config_options.copy()
+    config_options.update(hdf5_groupname=SHARED_PARAMS,
+                          bands=SHARED_PARAMS,
+                          ref_band=SHARED_PARAMS,
+                          redshift_col=SHARED_PARAMS,
+                          convert_to_colors=Param(bool, True, msg="if True, keep ref_mag and convert other cols to colors"),
+                          include_odds=Param(bool, False, msg="if True, compute ODDS and include in NN"),
+                          nodecounts=Param(list, default_node_counts, msg="number of notes in NN"),
+                          splitnum=Param(int, 5, msg="number of neural nets to train"),
+                          activations=Param(list, default_activations, msg="list of activation functions for NN"),
+                          epochs=Param(int, 1000, msg="max number of training epochs for NN"),
+                          acc_cutoff=Param(float, 0.07, msg="boundary value of dz/1+z used for good/bad in NN"),
+                          zphot_name=Param(str, "zmode", msg="name of point estimate to grab from PDF ancil data"),
+                          trainfrac=Param(float, 0.75, msg="fraction of data to keep train vs validation in model creation"),
+                          seed=Param(int, 1234, msg="seed for numpy"),
+                          )
+    inputs = [('pdfs', QPHandle),
+               ('input', TableHandle)]
+
+    outputs = [('model', ModelHandle)]
+
+
+    def __init__(self, args, **kwargs):
+        """Init function, init config stuff
+        """
+        super().__init__(args, **kwargs)
+        self.nodecounts = self.config.nodecounts
+        self.activations = self.config.activations
+        self.epochs = self.config.epochs
+        self.splitnum = self.config.splitnum
+        # self.correct_pz = self.config.correct_pz
+        # self.data_name = data_name
+        self.acc_cutoff = self.config.acc_cutoff
+        self.odds = None
+
+    def _compute_colors(self, data, refband, bands):
+        nbands = len(bands)
+        if self.config.include_odds:
+            print("including ODDS")
+            npar = nbands + 1
+        else:
+            npar = nbands
+        print(f"nbands: {nbands}, npar: {npar}")
+        colordata = np.zeros([npar, self.ngal])
+        colordata[0,:] = data[refband]
+        for i in range(nbands - 1):
+            colordata[i+1, :] = data[bands[i]] - data[bands[i+1]]
+        if self.odds is not None:
+            colordata[npar - 1, :] = self.odds
+        print(f"colordata shape in train: {colordata.shape}")
+        return colordata.T
+
+    def _split_data(self, data, szdata, pzdata, trainfrac, seed):
+        """
+        make a random partition of the training data into training and
+        validation, validation data will be used to determine bump
+        thresh and sharpen parameters.
+        """
+        nobs = data.shape[0]
+        ntrain = round(nobs * trainfrac)
+        # set a specific seed for reproducibility
+        rng = np.random.default_rng(seed=seed)
+        perm = rng.permutation(nobs)
+        self.features_train = data[perm[:ntrain], :]
+        self.trainsz = szdata[perm[:ntrain]]
+        self.trainpz = pzdata[perm[:ntrain]]
+        self.features_val = data[perm[ntrain:], :]
+        self.valsz = szdata[perm[ntrain:]]
+        self.valpz = pzdata[perm[ntrain:]]
+        # return x_train, x_val, z_train, z_val, p_train, p_val
+
+
+    def inform(self, input_data, pdf_data):
+        """need a custom inform because this class has two inputs"""
+        self.set_data("input", input_data)
+        self.set_data("pdfs", pdf_data)
+        self.run()
+        self.finalize()
+        return self.get_handle("model")
+
+
+    def meanreprocess(self, X):
+        return (X - self.feature_avgs)/np.sqrt(self.feature_vars)
+    
+    def run(self):
+        """compute the best fit prior parameters
+        """
+
+        # convert training data format to numpy dictionary
+        #if tables_io.types.table_type(input) != 1:
+        #    photom_data = self._convert_table_format(photom_data, out_fmt_str="numpyDict")
+
+        if self.config.hdf5_groupname:
+            photom_data = self.get_data("input")[self.config.hdf5_groupname]
+        else:  # pragma: no cover
+            photom_data = self.get_data("input")
+        self.ngal = len(photom_data[self.config.ref_band])
+
+        # grab the photoz point estimate from PDF ancil data
+        ens = self.get_data("pdfs")
+
+        ancilkeys = ens.ancil.keys()
+        if self.config.zphot_name not in ancilkeys:
+            raise KeyError(f"zphot_name {self.config.zphot_name} not present in qp ancil keys!")
+        else:
+            zphot = ens.ancil[self.config.zphot_name].flatten()
+        
+        if self.config.include_odds:
+            lowz = zphot - .06 * (1. + zphot)
+            hiz = zphot + 0.06 * (1 + zphot)
+            self.odds = np.array([ens[i].cdf(hiz[i]) - ens[i].cdf(lowz[i]) for i in range(ens.npdf)])
+        else:
+            self.odds = None
+        
+        if self.config.convert_to_colors:
+            if self.config.ref_band not in self.config.bands or self.config.ref_band not in photom_data.keys():
+                raise KeyError(f"ref_band {self.config.ref_band} not in bands list!")
+            else:
+                color_data = self._compute_colors(photom_data, self.config.ref_band, self.config.bands)
+        else:  # pragma: no cover
+            raise ValueError("non color data not implemented")
+
+        if self.config.redshift_col not in photom_data.keys():
+            raise KeyError(f"redshift column {self.config.redshift_col} not present in input data!")
+        else:
+            specz = photom_data[self.config.redshift_col]
+        
+        
+        # ngal = len(photom_data[self.config.ref_band])
+
+        self._split_data(color_data, specz, zphot, self.config.trainfrac, self.config.seed)
+
+        self.is_goodfit_train = (np.abs(self.trainpz - self.trainsz) < (1. + self.trainsz)*self.acc_cutoff)
+        self.is_goodfit_val = (np.abs(self.valpz - self.valsz) < (1. + self.valsz)*self.acc_cutoff)
+
+        # Now, actually train the models, stuff from the create_models function in the original code
+        self.nnlist = []
+        for i in range(self.splitnum):
+            xmodel = Sequential()
+            xmodel.add(Dense(self.nodecounts[0], activation=self.activations[0],
+                             input_shape = (color_data.shape[1],)))
+            for xnode, xact in zip(self.nodecounts[1:], self.activations[1:]):
+                if xact == 'selu':
+                    kernel_init = 'lecun_normal'
+                else:
+                    kernel_init = 'glorot_uniform'
+                xmodel.add(Dense(xnode, activation=xact, kernel_initializer=kernel_init))
+            initial_learning_rate = 0.005  # this is hardcoded in original code!
+            lr_schedule = ExponentialDecay(initial_learning_rate,
+                                           decay_steps=3500,
+                                           decay_rate=0.1,
+                                           staircase=False)
+            xmodel.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=lr_schedule), metrics=['accuracy'])
+            self.nnlist.append(xmodel)
+        # back to get_models() stuff
+        self.feature_avgs = np.average(self.features_train, axis = 0)
+        self.feature_vars = np.var(self.features_train, axis = 0)
+        for x, thismodel in enumerate(self.nnlist):
+            x_train = self.meanreprocess(self.features_train)
+            y_train = self.is_goodfit_train.reshape(-1,1)
+            x_val = self.meanreprocess(self.features_val)
+            y_val = self.is_goodfit_val.reshape(-1,1)
+
+            es = EarlyStopping(patience=25, restore_best_weights=True)
+            history = thismodel.fit(x_train, y_train, batch_size = 1000, epochs = self.epochs, verbose = 0,
+                                    validation_data = (x_val, y_val), callbacks = [es])
+            #this_fit_val = np.squeeze(thismodel.predict(self.meanreprocess(self.features_val), verbose = 0))
+            #predictions = np.squeeze(thismodel.predict(self.meanreprocess(self.features_val)))
+
+        predictions = []
+        for xmod in self.nnlist:
+            predictions.append(xmod.predict(x_val, verbose=1))
+        singlepred = np.average(predictions, axis=0)
+
+        self.model = dict(feature_avgs=self.feature_avgs,
+                          feature_vars=self.feature_vars,
+                          xval=x_val,
+                          valsz=self.valsz,
+                          valpz=self.valpz,
+                          nnlist=self.nnlist,
+                          include_odds=self.config.include_odds,
+                          singlepred=singlepred)
+
+        
+        self.add_data("model", self.model)
+
+
+class NNFlagEstimator(CatEstimator):
+    """Estimator stage, takes model file that includes a set of trained NNCs
+    and calculates a flag for each input PDF to see how likely that PDF is to
+    be a good fit.  I'm going to ignore a bunch of the options and just do the
+    base computation where it predicts the flag and avearges them, which is
+    the default behavior in the example script
+    """
+    name = "NNFlagEstimator"
+    config_options = CatEstimator.config_options.copy()
+    config_options.update(bands=SHARED_PARAMS,
+                          hdf5_groupname=SHARED_PARAMS,
+                          ref_band=SHARED_PARAMS,
+                          zphot_name=Param(str,"zmode", msg="name for point estimate ancil data to use"),
+                          convert_to_colors=Param(bool, True, msg="if True, keep ref_mag and convert other cols to colors"),                          
+                         )
+    inputs = [('pdfs', QPHandle),
+              ('input', TableHandle),
+              ('model', ModelHandle)]
+    outputs = [('output', TableHandle)]
+
+    def __init__(self, args, **kwargs):
+        """Constructor, build the CatEstimator, then do BPZ specific setup
+        """
+        super().__init__(args, **kwargs)
+        if self.config.ref_band not in self.config.bands:
+            raise ValueError("ref_band not present in bands list! ")
+        self.odds = None
+
+    def open_model(self, **kwargs):
+        CatEstimator.open_model(self, **kwargs)
+        self.modeldict = self.model
+        self.knnmodels = self.modeldict['nnlist']
+        self.feature_avgs = self.modeldict['feature_avgs']
+        self.feature_vars = self.modeldict['feature_vars']
+        self.include_odds = self.modeldict['include_odds']
+        if self.include_odds:
+            print("include_odds set to True in model, will calculate ODDS")
+        
+    def meanreprocess(self, X):
+        return (X - self.feature_avgs)/np.sqrt(self.feature_vars)
+
+    def _compute_colors(self, data, refband, bands):
+        nbands = len(bands)
+        if self.odds is not None:
+            npar = nbands + 1
+        else:
+            npar = nbands
+        print(f"npar: {npar}")
+        colordata = np.zeros([npar, self.ngal])
+        colordata[0,:] = data[refband]
+        for i in range(nbands - 1):
+            colordata[i+1, :] = data[bands[i]] - data[bands[i+1]]
+        if self.odds is not None:
+            colordata[npar - 1, :] = self.odds
+        return colordata.T
+
+    def estimate(self, input_data, pdf_data):
+        self.open_model(**self.config)
+        self.set_data("input", input_data)
+        self.set_data("pdfs", pdf_data)
+        self.validate()
+        self.run()
+        self.finalize()
+        results = self.get_handle("output")
+        results.read(force=True)
+        return results
+
+        
+    #    def _process_chunk(self, start, end, data, first):
+    def run(self):
+        """
+        Run flagger on 
+        """
+
+        #qp_dstn = qp.Ensemble(qp.interp, data=dict(xvals=self.zgrid, yvals=pdfs))
+        #qp_dstn.set_ancil(dict(zmode=zmode, zmean=zmean, tb=tb, todds=todds))
+        #self._do_chunk_output(qp_dstn, start, end, first, data=data)
+
+        if self.config.hdf5_groupname:
+            photom_data = self.get_data("input")[self.config.hdf5_groupname]
+        else:  # pragma: no cover
+            photom_data = self.get_data("input")
+        self.ngal = len(photom_data[self.config.ref_band])
+
+        if self.include_odds:
+            ens = self.get_data("pdfs")
+            ancilkeys = ens.ancil.keys()
+            if self.config.zphot_name not in ancilkeys:
+                raise KeyError(f"zphot_name {self.config.zphot_name} not present in qp ancil keys!")
+            else:
+                zphot = ens.ancil[self.config.zphot_name].flatten()
+        
+            lowz = zphot - .06 * (1. + zphot)
+            hiz = zphot + 0.06 * (1 + zphot)
+            self.odds = np.array([ens[i].cdf(hiz[i]) - ens[i].cdf(lowz[i]) for i in range(ens.npdf)])
+        else:
+            self.odds = None
+
+        if self.config.convert_to_colors:
+            if self.config.ref_band not in self.config.bands or self.config.ref_band not in photom_data.keys():
+                raise KeyError(f"ref_band {self.config.ref_band} not in bands list!")
+            else:
+                color_data = self._compute_colors(photom_data, self.config.ref_band, self.config.bands)
+        else:  # pragma: no cover
+            raise ValueError("non color data not implemented")
+        white_color_data = self.meanreprocess(color_data)
+
+        predictions = []
+        for xmod in self.knnmodels:
+            predictions.append(xmod.predict(white_color_data, verbose=1))
+        singlepred = np.average(predictions, axis=0)
+        outdict = dict(nncflag=singlepred)
+        self.add_data("output", outdict)
+                               
